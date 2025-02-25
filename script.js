be06226c6ac6fa4bbb07be4f1fb71406239638d7
hatim <hatim2.shaherawala@live.uwe.ac.uk>
2025-02-25 21:46:34 +0000

    test5

--- script.js
+++ script.js
@@ -33,6 +33,9 @@ function unlockLetter(letterNumber, correctAnswer) {
 
     if (userAnswer === correctAnswer) {
         document.getElementById(linkId).style.display = "block";
+
+        // Store a version number (change when you update the letter)
+        localStorage.setItem(`letter${letterNumber}_unlocked`, "v2");
     } else {
         alert("Try again, my love! 💕");
     }
